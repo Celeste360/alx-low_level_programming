06e6161c9e78a7acb3e3839b5dde456093c4c3b7
Celeste360 <ceeborsor@gmail.com>
2023-06-16 00:16:40 +0100

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,11 @@
 /**
 * main -Entry point
 *
-* Description: prints "with proper grammar, but the outcome is a place
-* of art,"
+* Description: prints "with proper grammar, but the outcome is a place of art,"
+*
 * Return: Always 0 (success)
 */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
