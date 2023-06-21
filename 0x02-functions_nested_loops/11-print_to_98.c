5f9a9e89447424874fd43e8d012be5bcca0e2b76
Celeste360 <ceeborsor@gmail.com>
2023-06-21 05:41:23 +0100

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,24 +14,31 @@ void print_to_98(int n)
 	{
 		for (; n <= 98; n++)
 		{
-			printf("%d", n);
-
 			if (n == 98)
-				continue;
-			printf(", ");
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+				printf("%d, ", n);
 		}
-		printf(", ");
-	}
-	else
-	{
-		for (; n >= 98; n--)
-		{
-			printf("%d", n);
-
-			if (n == 98)
-				continue;
-			printf(", ");
-		}
-		printf("\n");
 	}
 }
+else
+{
+	for (' n >= 98; n--)
+	{
+		if (n == 98)
+		{
+			printf("%d", n);
+			printf("\n");
+			break;
+		}
+		else
+		{
+			printf("%d, ", n);
+		}
+	}
+}
+				
