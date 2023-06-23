47189cd3650ef974c3c8700ca5f0f2ef7e70caa1
Celeste360 <ceeborsor@gmail.com>
2023-06-23 01:47:33 +0100

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,9 @@ int main(void)
 {
 	int x;
 
-	for (x = 1; x <= 100; x++)
+	x = 1;
+	printf("%d", x);
+	for (x = 2; n <= 100; x++)
 	{
 		if ((x % 3 == 0) && (x % 5 == 0))
 		{
@@ -21,18 +23,12 @@ int main(void)
 		else if (x % 5 == 0)
 		{
 			printf("Buzz");
-		{
+		}
 		else
 		{
 			printf("%d", x);
 		}
-
-		if (x != 100)
-		{
-			printf(" ");
-		}
 	}
 	printf("\n");
-
 	return (0);
 }
