912b66e02ebddc1c10bd5f4ba32eb5e3e274f5f0
Celeste360 <ceeborsor@gmail.com>
2023-06-26 07:36:05 +0100

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenght of a string
+ * @a: input string
+ * Return: length of a string
+ */
+int _strlen(char *a)
+{
+	int count = 0
+
+	while (*(a + count) != '\0')
+		count++;
+	return (count);
+}
