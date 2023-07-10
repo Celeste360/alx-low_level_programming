4caf4f3b2978eb377cfbf3a0f45cf643f86f94c6
Celeste360 <ceeborsor@gmail.com>
2023-07-11 00:15:02 +0100

    101-strtow.c

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -41,7 +41,7 @@ char **strtow(char *str)
 
 	while (*(str + len))
 		len++;
-	words == count_word(str);
+	words = count_word(str);
 	if (words == 0)
 		return (NULL);
 
@@ -49,7 +49,7 @@ char **strtow(char *str)
 	if (matrix == NULL)
 		return (NULL);
 
-	for (i = o; i <= len; i++)
+	for (i = 0; i <= len; i++)
 	{
 		if (str[i] == ' ' || str[i] == '\0')
 		{
